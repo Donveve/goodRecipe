09302b7f93e8dc598516b34abffdaa561cd939cc
Veniel Dorestave <veniel@noukod.com>
2019-03-29 16:22:45 +0000

    gem added

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
-  root "categories#index"
-  resources :categories, only: [:index, :show]
-  devise_for :users
-  resources :users, :only =>[:show]
-  resources :recipes
-  post 'recipes/:id/favorite', to: 'recipes#toggle_favorite', as: 'favorite_recipe'
-  post 'recipes/:id/rate', to: 'recipes#add_rating', as: 'rate_recipe'
-  
-  get 'recipes/new/ingredient', to: 'recipes#ingredient'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.htmlend
+  # root "categories#index"
+  # resources :categories, only: [:index, :show]
+  # devise_for :users
+  # resources :users, :only =>[:show]
+  # resources :recipes
+  # post 'recipes/:id/favorite', to: 'recipes#toggle_favorite', as: 'favorite_recipe'
+  # post 'recipes/:id/rate', to: 'recipes#add_rating', as: 'rate_recipe'
+  #
+  # get 'recipes/new/ingredient', to: 'recipes#ingredient'
+end
